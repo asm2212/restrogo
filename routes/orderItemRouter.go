86ea5a306331bed5492d38b6c680bb17cd9209f3
asm2212 <asmareadmasu0@gmail.com>
2025-06-08 11:34:33 +0300

    Add MongoDB connection setup and update order item routes

--- routes/orderItemRouter.go
+++ routes/orderItemRouter.go
@@ -8,6 +8,7 @@ import (
 func OrderItemRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.GET("/orderItems", controller.GetOrderItems())
 	incomingRoutes.GET("/orderItems/:orderItem_id", controller.GetOrderItem())
+	incomingRoutes.GET("/orderItems-order/:order_id", controller.GetOrderItemsByOrder())
 	incomingRoutes.POST("/orderItems", controller.CreateOrderItem())
-	incomingRoutes.POST("/orderItems/:orderItem_id", controller.UpdateOrderItem())
+	incomingRoutes.PATCH("/orderItems/:orderItem_id", controller.UpdateOrderItem())
 }
