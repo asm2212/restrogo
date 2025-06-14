f0810152fc875887144033a6ea3407c4422b1a95
asm2212 <asmareadmasu0@gmail.com>
2025-06-14 14:14:03 +0300

    order item

--- controllers/orderItemController.go
+++ controllers/orderItemController.go
@@ -1,40 +1,179 @@
 package controllers
 
 import (
+	"context"
+	"net/http"
+	"time"
+
+	"golang-restrogo/database"
+	"golang-restrogo/models"
+
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
+
+var orderItemCollection *mongo.Collection = database.OpenCollection(database.Client, "orderItems")
+var validate = validator.New()
+
+type OrderItemPack struct {
+	Table_id    *string            `json:"table_id" validate:"required"`
+	Order_items []models.OrderItem `json:"order_items" validate:"required,dive"`
+}
 
 func GetOrderItems() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
 
+		result, err := orderItemCollection.Find(ctx, bson.M{})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error listing order items"})
+			return
+		}
+		var allOrderItems []bson.M
+		if err = result.All(ctx, &allOrderItems); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error decoding order items"})
+			return
+		}
+		c.JSON(http.StatusOK, allOrderItems)
 	}
 }
 
 func GetOrderItemsByOrder() gin.HandlerFunc {
 	return func(c *gin.Context) {
-
+		orderID := c.Param("order_id")
+		allOrderItems, err := ItemsByOrder(orderID)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, allOrderItems)
 	}
 }
 
-func ItemsByOrder(id string) (OrderItems []primitive.M, err error) {
+func ItemsByOrder(id string) (orderItems []primitive.M, err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 
+	filter := bson.M{"order_id": id}
+	cursor, err := orderItemCollection.Find(ctx, filter)
+	if err != nil {
+		return nil, err
+	}
+	if err = cursor.All(ctx, &orderItems); err != nil {
+		return nil, err
+	}
+	return orderItems, nil
 }
 
 func GetOrderItem() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
 
+		orderItemID := c.Param("order_item_id")
+		objID, _ := primitive.ObjectIDFromHex(orderItemID)
+
+		var orderItem models.OrderItem
+
+		err := orderItemCollection.FindOne(ctx, bson.M{"_id": objID}).Decode(&orderItem)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Order item not found"})
+			return
+		}
+		c.JSON(http.StatusOK, orderItem)
 	}
 }
 
 func CreateOrderItem() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+
+		var OrderItemPack OrderItemPack
+		var order models.Order
+
+		if err := c.BindJSON(&OrderItemPack); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		order.Order_Date, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+
+		orderItemsToBeInserted := []interface{}{}
+		order.Table_id = OrderItemPack.Table_id
+		order_id := OrderItemOrderCreator(order)
+
+		for _, orderItem := range OrderItemPack.Order_items {
+			orderItem.OrderID = order_id
+
+			validationErr := validate.Struct(orderItem)
+
+			if validationErr != nil {
+				c.JSON(http.StatusBadRequest, gin.H{"error": validationErr})
+				return
+			}
+
+			orderItem.ID = primitive.NewObjectID()
+			orderItem.CreatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+			orderItem.UpdatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+			orderItem.OrderItemID = orderItem.ID.Hex()
+			var num = toFixed(*orderItem.UnitPrice, 2)
+			orderItem.UnitPrice = &num
+			orderItemsToBeInserted = append(orderItemsToBeInserted, orderItem)
+
+		}
+
+		insertedOrderItems, err := orderItemCollection.InsertMany(ctx, orderItemsToBeInserted)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Order item creation failed"})
+			return
+		}
+		defer cancel()
+
+		c.JSON(http.StatusCreated, insertedOrderItems)
 	}
 }
 
 func UpdateOrderItem() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		id := c.Param("id")
+		objID, _ := primitive.ObjectIDFromHex(id)
 
+		var updateData models.OrderItem
+		if err := c.ShouldBindJSON(&updateData); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		if err := validate.Struct(updateData); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		update := bson.M{
+			"$set": bson.M{
+				"quantity":      updateData.Quantity,
+				"unit_price":    updateData.UnitPrice,
+				"updated_at":    time.Now(),
+				"food_id":       updateData.FoodID,
+				"order_item_id": updateData.OrderItemID,
+				"order_id":      updateData.OrderID,
+			},
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		_, err := orderItemCollection.UpdateOne(ctx, bson.M{"_id": objID}, update)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Order item update failed"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"message": "Order item updated successfully"})
 	}
 }
--- models/orderModel.go
+++ models/orderModel.go
@@ -9,10 +9,8 @@ import (
 type Order struct {
 	ID         primitive.ObjectID `bson:"_id,omitempty"`
 	Order_id   string             `json:"order_id"`
-	Table_id   string             `json:"table_id" validate:"required"`
-	Items      []string           `json:"items" validate:"required"` // slice of food_ids
-	Ordered_at time.Time          `json:"ordered_at"`
+	Table_id   *string            `json:"table_id" validate:"required"`
+	Order_Date time.Time          `json:"ordered_date"`
 	Updated_at time.Time          `json:"updated_at"`
 	Created_at time.Time          `json:"created_at"`
-	Price      float64            `json:"price"`
 }
